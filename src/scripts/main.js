a8c993d88004bf9152195176d6abe112885b63fe
Bruno Da Silva Nobre <developnobre@gmail.com>
2024-09-24 15:49:45 -0300

    ok

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,5 @@
 AOS.init();
-const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
+const dataDoEvento = new Date("August 19, 2026 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const tempoEspera = setInterval(function(){
